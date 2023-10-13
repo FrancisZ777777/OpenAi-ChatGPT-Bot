27ff985402b5c2d30381f1b350554e79ef9059e6
FrancisZ777777 <146390754+FrancisZ777777@users.noreply.github.com>
2023-10-13 10:02:11 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,11 +6,11 @@ const chalk = require('chalk')
 🤖 𝗕𝗢𝗧 𝗦𝗘𝗧𝗧𝗜𝗡𝗚𝗦
 */
 global.OFFLINE = false // if you want put bot offline put true
-global.BOT_PRIVATE = true // if you want use bot privet 
+global.BOT_PRIVATE = false // if you want use bot privet 
 global.AUTO_CHATGPT = false // if you want auto ai bot mod
 global.READ_MESSAGE = false // if you want read message from bot
 global.ONLY_INBOX = false // Only inbox mod ( only pm ) use
-global.ONLY_GROUPS = true // Only Groups Use
+global.ONLY_GROUPS = false // Only Groups Use
 
 
 /*
@@ -20,7 +20,7 @@ global.OWNER_NAME = 'FrancisZ' // Owner Name
 global.OWNER_NUMBER = ['255657606238'] //Owner Number
 global.BOT_ADMINS = ['255657606238'] //Bot admins numbers Like a Sudo Numbers
 global.BLOCK_CHAT = [] // If you want Block Bot From Chats Use .jid Command and put Jids
-global.IMAGE_QUALITY = 'low' // Available Quality Are => low | mediam | high
+global.IMAGE_QUALITY = 'high' // Available Quality Are => low | mediam | high
 global.CAPTION = '*ZWISKY TECH*'
 
 /*
